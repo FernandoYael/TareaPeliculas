3eb13e21a5f78166e0c2a527b4e232e86b94fc70
Demonear <126305470+FernandoYael@users.noreply.github.com>
2023-03-21 05:51:41 +0000

    Hola

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -8,39 +8,75 @@ namespace Pelicula
     {
         //Campos
         private List<Actor> actores = new List<Actor>();
-        public string titulo;
-        public Int16 año
+        private short año{ get; set; }
+        private string? país{ get; set; }
+        private string? director{ get; set; }
+        private string? titulo{ get; set; }
+
         //Constructores
         public Pelicula()
         {
-
         }
-        public Pelicula(string titulo, Int16 año)
+        public Pelicula(string titulo, short año)
+            : this()
         {
-            
+            this.titulo = titulo;
+            this.año = año;
         }
+
      
         //Métodos
      
+        public void SetTitulo(string titulo)
+        {
+            this.titulo = titulo;
+            }
+        public void SetAño(short año)
+        {
+            this.año = año;
+        }
+        public string? GetTitulo()
+        {
+            return this.titulo;
+        }
+        public short GetAño()
+        {
+            return this.año;
+        }
+        public void AgregaActor(Actor actor)
+        {
+            actores.Add(actor);
+        }
         public void Imprime()
         {
             Console.WriteLine($"{titulo} ({año})");
         }
-
-
+        public void ImprimeActores()
+        {
+            foreach (Actor actor in actores)
+            {
+                actor.Imprime();
+            }
+        }
     }
 
     public class Actor
     {
         //Propiedades
-
+        private string? nombre{ get; set; }
+        private short año{ get; set; }
+        public Actor(string nombre, short año)
+        {
+            this.nombre = nombre;
+            this.año = año;
+        }
         //Constructores
 
 
         //Métodos 
         public void Imprime()
         {
-            Console.WriteLine($"{Nombre} ({Año})");
+            Console.WriteLine($"{nombre} ({año})");
         }
     }
 
@@ -56,7 +92,11 @@ namespace Pelicula
             Pelicula p1 = new Pelicula();
             p1.SetTitulo("El Gato con Botas 2");
             p1.SetAño(2022);
-            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+            Pelicula p2 = new Pelicula();
+            p2.SetTitulo("El Viaje de Chihiro");
+            p2.SetAño(2001);
+            p1.Imprime();
+            p2.Imprime();
         }
     }
 }
